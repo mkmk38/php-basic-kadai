b3e42c0df47ebaa36cd1114b9c8e00605e4619e2
mkmk38 <mtnb03@gmail.com>
2024-11-14 22:31:15 +0900

    「kadai_007」フォルダをアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+  <html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+        // 連想配列を作成し、変数に代入する
+        $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 連想配列の値を出力する
+        print_r($product_data);
+
+      ?>
+
+    </p>
+  </body>
+
+  </html>
